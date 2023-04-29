831a0550b5701adec9e260c4f83a148b503d0ddf
Yodgorbek <yodgorbeksheraliyev777@gmail.com>
2023-04-29 09:30:52 +0500

    lesson-51

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
-import Dish from "./Components/Dishes/Dish";
-import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
-import Menu from "./Components/Header/Menu/Menu";
-import Testimony from "./Components/Header/Testimony/Testimony";
-import News from "./Components/News/News";
-import Letter from "./Components/Newsletter/Letter";
-
 function App() {
   return (
     <div className="App">
-   <Header/>
-   <Dish/>
-   <Menu/>
-   <Testimony/>
-   <News/>
-   <Letter/>
-   <Footer/>
     </div>
   );
 }
