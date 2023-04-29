5eaca79b25a0e99fad09f8374e07e1ae076ccf07
Yodgorbek <yodgorbeksheraliyev777@gmail.com>
2023-04-29 11:19:39 +0500

    todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { ToDo } from "./Components/ToDo/ToDo";
+import { useState } from "react";
 
 function App() {
+  const localTodos = localStorage.getItem('todos');
+  const [todos, setTodos] = useState(localTodos ? JSON.parse(localTodos): [ ])
+
   return (
     <div className="App">
-      <ToDo/>
+      <Form todos={todos} setTodos={setTodos}></Form>
+      <Todos todos={todos} setTodos={setTodos}></Todos>
     </div>
   );
 }
--- src/Components/ToDo/ToDo.jsx
+++ src/Components/ToDo/ToDo.jsx
@@ -1,48 +1,27 @@
-import React, {useState} from 'react';
-import './todo.css';
+export const Todos = ({todos, setTodos}) => {
+
+    const handleDeleteTodo = (id) => {
+        const filteredTodos = todos.filter(todo => todo.id !== id);
+        setTodos(filteredTodos);
+        localStorage.setItem('todos', JSON.stringify(filteredTodos))
+    }
+    
+    const handleEditTodo = (todo) => {
+        const editText = prompt('Edit !', todo.text);
+        const foundIndex = todos.findIndex((item) => item.id === id);
+        todos[foundIndex].text = editText;
+        setTodos([...todos])
+        localStorage.setItem('todos', JSON,stringify(todos));
+    }
+
+    const handleCompletedTodo = (evt, id) => {
+        const foundIndex = todos.findIndex(todo => todo.id === id);
+        todos[foundIndex].isCompleted = evt.target.checked;
+        setTodos([...todos])
+    }
 
 
-export function ToDo() {
-    const [firstname, setFirstname] = useState('');
-    const [lasttname, setLastname] = useState('');
-    const [username, setUsername] = useState('');
-    const [age, setAge] = useState('');
-    const [salary, setSalary] = useState('');
-    const [showModal, setShowModal] = useState( )
-  return(
-       <div className="user">
-        <h2 className="user__title">User details</h2>
-        <button className='add'>Add User</button>
-        <form action="#" className="form">
-            <h3 className='form__title'>User details</h3>
-           <label >Firstname</label>
-           <input type="text" />
-           <label >Lastname</label>
-           <input type="text" />
-           <label >Username</label>
-           <input type="text" />
-           <label >Age</label>
-           <input type="number" />
-           <label >Salary</label>
-           <input type="text" />
-           <button type="submit" className='submit'>Submit</button>
-        </form>
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Firstname</th>
-                    <th>Lastname</th>
-                    <th>Username</th>
-                    <th>Age</th>
-                    <th>Salary</th>
-                    <th>Change</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr></tr>
-            </tbody>
-        </table>
-       </div>
-  )
+
+
+
 }
