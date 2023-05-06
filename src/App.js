38856173ddd583df3f4e41a2829846a21a36d8cb
Yodgorbek <yodgorbeksheraliyev777@gmail.com>
2023-05-06 22:13:29 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
-import Routing from "./Router/Routing";
+import { useContext } from "react";
+import { MyContext } from "./Components/ExampleContext";
 
 function App() {
+  const {count, handleCount} = useContext(MyContext);
   return (
     <div className="App">
-      <Routing/>
+      <h2>{count}</h2>
+      <button>Add me</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { MyProvider } from "./Components/ExampleContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
     <BrowserRouter>
-      <App />
+      <MyProvider>
+       <App />
+      </MyProvider>
     </BrowserRouter>
     </Suspense>
   </React.StrictMode>
