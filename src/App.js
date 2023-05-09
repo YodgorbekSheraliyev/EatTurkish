e46a4bc6def289b7aedf301567bb9140d8d6c6a7
Yodgorbek <yodgorbeksheraliyev777@gmail.com>
2023-05-09 10:30:26 +0500

    modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
 import "./App.css";
-import { useContext } from "react";
-import { MyContext } from "./Components/ExampleContext";
 
 function App() {
-  const {count, increaseCount, decreaseCount} = useContext(MyContext);
   return (
     <div className="App">
-      <h2>{count}</h2>
-      <button onClick={()=>{increaseCount()}}>Add one</button>
-      <button onClick={()=>{decreaseCount()}}>Substract one</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { MyProvider } from "./Components/ExampleContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
-    <BrowserRouter>
-      <MyProvider>
        <App />
-      </MyProvider>
-    </BrowserRouter>
     </Suspense>
   </React.StrictMode>
 );
-
-reportWebVitals();
